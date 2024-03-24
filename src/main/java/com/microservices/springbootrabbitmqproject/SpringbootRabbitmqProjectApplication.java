f321934ecceac04bdfb727174a9351e9aa3b5fe4
manshi rathore <manshirathore555@gmail.com>
2024-03-24 22:27:12 +0530

    initial commit

---
+++ src/main/java/com/microservices/springbootrabbitmqproject/SpringbootRabbitmqProjectApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.springbootrabbitmqproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRabbitmqProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservices/springbootrabbitmqproject/SpringbootRabbitmqProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.springbootrabbitmqproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRabbitmqProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
